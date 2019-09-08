9fc2b0a66d1a7c8b1d334d956e06c562dc57ebd1
eglaab <49523224+eglaab@users.noreply.github.com>
2019-09-08 15:36:06 +0200

    Update elixir_day2.R

--- elixir_day2.R
+++ elixir_day2.R
@@ -14,13 +14,91 @@
 # - Install the current version of R-Studio (1.2) from: https://www.rstudio.com/products/rstudio/download/
 #
 
+#
+# Installation and loading of R-packages
+#
+
+## Default repo
+local({r <- getOption("repos")
+       r["CRAN"] <- "https://cloud.r-project.org" 
+       options(repos=r)
+})
+
+update.packages(ask = FALSE)
+
+# load annotation package for gene ID conversion
+
+# for old R version:
+# source("http://bioconductor.org/biocLite.R")
+# biocLite("hgu133a.db")
+
+if(!require('hgu133a.db'))
+{
+	if (!requireNamespace("BiocManager", quietly = TRUE))
+	    install.packages("BiocManager")
+	BiocManager::install("hgu133a.db", suppressUpdates=TRUE)
+	require('hgu133a.db')
+}
+
+# load R-packages for quality control
+
+if(!require('arrayQualityMetrics'))
+{
+  if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+  BiocManager::install("arrayQualityMetrics", suppressUpdates=TRUE)
+  install.packages("gridSVG")
+  # install.packages("https://cran.r-project.org/src/contrib/Archive/gridSVG/gridSVG_1.4-3.tar.gz", repos=NULL)
+  require('arrayQualityMetrics')
+}
+if(!require('Biobase'))
+{
+  if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+  BiocManager::install("Biobase", suppressUpdates=TRUE)
+  require('Biobase')
+}
+
+# load R-packages for power calculation
+if(!require('impute'))
+{
+ if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+ BiocManager::install("impute", suppressUpdates=TRUE)
+ require('impute')
+}
+
+if(!require('samr'))
+{
+  install.packages("samr")
+  require('samr')
+}
+options(error=NULL)
+
+# load R-package for Variance stabilizing normalization
+if(!require('vsn'))
+{
+	if (!requireNamespace("vsn", quietly = TRUE))
+	    install.packages("BiocManager")
+	BiocManager::install("vsn", suppressUpdates=TRUE)
+	require('vsn')
+}
+
+# load R-package for meta-analysis
+if(!require('metaMA'))
+{
+	install.packages('metaMA')
+	require('metaMA')
+}
+
 
 # install R-package for pathway analysis
 if(!require('clusterProfiler'))
 {
 	if (!requireNamespace("BiocManager", quietly = TRUE))
-	    install.packages("BiocManager")
-	
+	    install.packages("BiocManager", suppressUpdates=TRUE)
+
 	BiocManager::install("clusterProfiler")
 	require('clusterProfiler')
 }
@@ -29,32 +107,20 @@ if(!require('GSEABase'))
 {
 	if (!requireNamespace("BiocManager", quietly = TRUE))
 	    install.packages("BiocManager")
-	
-	BiocManager::install("GSEABase")
+
+	BiocManager::install("GSEABase", suppressUpdates=TRUE)
 	require('GSEABase')
 }
-
 
 # install Limma package for statistical analyses
 if(!require('limma'))
 {
 	if (!requireNamespace("BiocManager", quietly = TRUE))
 	    install.packages("BiocManager")
-	
-	BiocManager::install("limma")
+
+	BiocManager::install("limma", suppressUpdates=TRUE)
 	require('limma')
 }
-
-# install R annotation package
-if(!require('hgu133a.db'))
-{
-	if (!requireNamespace("BiocManager", quietly = TRUE))
-	    install.packages("BiocManager")
-	
-	BiocManager::install("hgu133a.db")
-	require('hgu133a.db')
-}
-
 
 # install R-packages for clustering
 if(!require('cluster'))
@@ -82,7 +148,9 @@ if(!require('e1071'))
 	require('e1071')
 }
 
-
+#
+# End of package installations
+#
 
 # set the location of your working directory (note that there are differences between Windows & Mac concerning the use of back slash "\" vs. forward slash "/")
 
